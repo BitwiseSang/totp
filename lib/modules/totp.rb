7d79bcf04d7db94e42b22f2985ddf1ba22c522ca
BitwiseSang <135348362+BitwiseSang@users.noreply.github.com>
2026-02-08 14:39:40 +0300

    feat: add verify method to check OTPs

--- lib/modules/totp.rb
+++ lib/modules/totp.rb
@@ -11,7 +11,24 @@ module TOTP
         TOTP::HOTP.generate(secret, counter: counter, digest: digest, digits: digits)
       end
 
+      def verified?(secret, code, interval: DEFAULT_INTERVAL, digest: DEFAULT_DIGEST, digits: DEFAULT_DIGITS)
+        counter = compute_counter(Time.now.to_s)
+
+        (-drift..drift).any? do |_iteration|
+          generated_code = generate(secret, counter + 1, interval: interval, digest: digest, digits: digits)
+          secure_compare(generated_code, code.to_s)
+        end
+      end
+
       private
+
+      def secure_compare(first_code, second_code)
+        return unless first_code.bytesize == second_code.bytesize
+
+        result = 0
+        first_code.bytes.zip(second_code.bytes) { |x, y| result |= (x ^ y) }
+        result.zero?
+      end
 
       def compute_counter(unix_time, interval)
         ((unix_time - 0) / interval).floor
