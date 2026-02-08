1fc4a87d5e7ff1831f34896c286947b869ed3ec2
BitwiseSang <135348362+BitwiseSang@users.noreply.github.com>
2026-02-08 12:04:25 +0300

    refactor(hmac): pass hash function instead of hardcoding

--- lib/modules/hmac.rb
+++ lib/modules/hmac.rb
@@ -1,69 +1,72 @@
 # frozen_string_literal: true
 
-require 'pry'
+require 'digest'
 
 module TOTP
   # This class defines the encryption method utitilized by TOTP
   class HMAC
     class << self
-      def raw_digest(secret_key, message)
+      def raw_digest(secret_key, message, digest: Digest::SHA1.new)
         # Convert input strings into an array of bytes for bitwise operations
         key_bytes = secret_key.bytes
         message_bytes = message.bytes
 
-        block_sized_key = compute_block_sized_key(key_bytes)
-        inner_padding = compute_inner_padding
-        outer_padding = compute_outer_padding
+        block_size = digest.block_length
+
+        block_sized_key = compute_block_sized_key(key_bytes, digest, block_size)
+        inner_padding = compute_inner_padding(block_size)
+        outer_padding = compute_outer_padding(block_size)
 
         inner_key = xor(block_sized_key, inner_padding)
         outer_key = xor(block_sized_key, outer_padding)
 
-        first_pass_result = first_pass(inner_key, message_bytes)
-        second_pass(outer_key, first_pass_result)
+        first_pass_result = first_pass(inner_key, message_bytes, digest)
+        second_pass(outer_key, first_pass_result, digest)
       end
 
-      def digest(secret_key, message)
+      def digest(secret_key, message, digest: Digest::SHA1.new)
         # Convert the array of bytes returned by raw digest into a binary string
-        raw_digest(secret_key, message).pack('C*')
+        raw_digest(secret_key, message, digest: digest).pack('C*')
       end
 
-      def hexdigest(secret_key, message)
+      def hexdigest(secret_key, message, digest: Digest::SHA1.new)
         # Convert the binary string from digest into a hex string
-        digest(secret_key, message).unpack1('H*')
+        digest(secret_key, message, digest: digest).unpack1('H*')
       end
 
       private
 
-      def first_pass(inner_key, message)
-        hash(concatenate(inner_key, message))
+      def first_pass(inner_key, message, digest)
+        hash(concatenate(inner_key, message), digest)
       end
 
-      def second_pass(outer_key, first_pass_result)
-        hash(concatenate(outer_key, first_pass_result))
+      def second_pass(outer_key, first_pass_result, digest)
+        hash(concatenate(outer_key, first_pass_result), digest)
       end
 
-      def hash(byte_array)
+      def hash(byte_array, digest)
         # Convert byte_array into a string before hashing
         # After hashing, convert the hashed string back into an array.
-        HASH_FUNCTION.call(byte_array.pack('C*')).bytes
+        digest.reset
+        digest.digest(byte_array.pack('C*')).bytes
       end
 
       def concatenate(first_byte_array, second_byte_array)
         [*first_byte_array, *second_byte_array]
       end
 
-      def pad(array, pad_with)
-        return array if array.length >= BLOCK_SIZE
+      def pad(array, pad_with, target_size)
+        return array if array.length >= target_size
 
-        padding = Array.new(BLOCK_SIZE - array.length) { pad_with }
+        padding = Array.new(target_size - array.length) { pad_with }
         array + padding
       end
 
-      def compute_block_sized_key(key_bytes)
-        if key_bytes.length > BLOCK_SIZE
-          pad(hash(key_bytes), 0)
-        elsif key_bytes.length < BLOCK_SIZE
-          pad(key_bytes, 0)
+      def compute_block_sized_key(key_bytes, digest, block_size)
+        if key_bytes.length > block_size
+          pad(hash(key_bytes, digest), 0, block_size)
+        elsif key_bytes.length < block_size
+          pad(key_bytes, 0, block_size)
         else
           key_bytes
         end
@@ -75,14 +78,14 @@ module TOTP
 
       # Block sized outer padding, consisting of repeated bytes value 0x5c (92
       # in decimal)
-      def compute_outer_padding
-        Array.new(BLOCK_SIZE) { 92 }
+      def compute_outer_padding(block_size)
+        Array.new(block_size) { 92 }
       end
 
       # Block sized inner padding, consisting of repeated bytes value 0x36 (54
       # in decimal)
-      def compute_inner_padding
-        Array.new(BLOCK_SIZE) { 54 }
+      def compute_inner_padding(block_size)
+        Array.new(block_size) { 54 }
       end
     end
   end
