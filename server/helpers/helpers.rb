c84b2c1ad56c642d4e3fd3baf3ac04a8dab48d21
BitwiseSang <135348362+BitwiseSang@users.noreply.github.com>
2026-02-23 19:37:42 +0300

    feat: import all the helpers as one module called SERVER

---
+++ server/helpers/helpers.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative 'uri_string'
+require_relative 'qrcode'
+
+module SERVER
+  # URI generation constants
+  USERNAME = 'RubyHacker'
+  EMAIL = 'admin@localhost'
+  ISSUER = 'RubyHacker'
+  ALGORITHM = 'SHA1'
+  DIGITS = 6
+  INTERVAL = 30
+end
